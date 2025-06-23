933ab43b5b156a6caa9c94ea93b1604cc696f9e2
azamat1012 <148556590+azamat1012@users.noreply.github.com>
2025-06-23 17:59:56 +0500

    Update models.py

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -199,7 +199,7 @@ class OrderItem(models.Model):
         max_digits=8,
         decimal_places=2,
         validators=[MinValueValidator(0)],
-        default=0.00,
+        blank=True,
         null=False
     )
 
